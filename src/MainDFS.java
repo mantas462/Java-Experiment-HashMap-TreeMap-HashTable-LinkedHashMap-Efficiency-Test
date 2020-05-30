a40c11026bb3016eac73b3520d14bb058e3a6e2d
iammantas <nacickasmantas@gmail.com>
2020-05-30 18:11:08 +0300

    Message

--- src/MainDFS.java
+++ src/MainDFS.java
@@ -1,10 +1,12 @@
+import java.util.HashMap;
+
 public class MainDFS {
 
     public static void main(String [] args) {
 
         long startTime=System.nanoTime();
 
-        System.out.println("eesseee");
+        HashMap<String, String> hashmap=new HashMap<String, String>();
 
         String first="m";
         String second="n";
@@ -12,11 +14,12 @@ public class MainDFS {
         for(int i=0; i<2000; i++) {
             first=first+"ms";
             second=second+"n";
-
+            hashmap.put(first,second);
         }
 
         long endTime=System.nanoTime();
         long timeElapsed=endTime-startTime;
         System.out.println(timeElapsed);
 
+    }
     }

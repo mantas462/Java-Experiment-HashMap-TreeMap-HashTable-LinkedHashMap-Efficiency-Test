d1ec33fbe5a7d5e609732048026ee37a63644581
iammantas <nacickasmantas@gmail.com>
2020-05-30 18:13:09 +0300

    TreeMap

--- src/MainDFS.java
+++ src/MainDFS.java
@@ -1,10 +1,13 @@
+import java.util.HashMap;
+import java.util.TreeMap;
+
 public class MainDFS {
 
     public static void main(String [] args) {
 
         long startTime=System.nanoTime();
 
-        System.out.println("eesseee");
+        TreeMap<String, String> treeMap=new TreeMap<String, String>();
 
         String first="m";
         String second="n";
@@ -12,11 +15,12 @@ public class MainDFS {
         for(int i=0; i<2000; i++) {
             first=first+"ms";
             second=second+"n";
-
+            treeMap.put(first,second);
         }
 
         long endTime=System.nanoTime();
         long timeElapsed=endTime-startTime;
         System.out.println(timeElapsed);
 
+    }
     }
